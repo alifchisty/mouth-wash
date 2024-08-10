c016b590174f66412031101f74d4ad4d067a0f8d
alifchisty <aichisty115@gmail.com>
2024-08-11 01:39:11 +0600

    fourth-short

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,8 @@
 const mongoose = require ("mongoose")
 
+const mongoURI = 'mongodb+srv://aichisty115:ANDIMANDIsandi123@first-short.i3qyh.mongodb.net/reliability?retryWrites=true&w=majority&appName=first-short';
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/getdata', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function() {
-    console.log('Connected to MongoDB');
-});
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('MongoDB connection error:', err));
